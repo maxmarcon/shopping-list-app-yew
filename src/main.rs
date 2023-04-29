45cfae645adb6d3ca79dca4b3535a49f10ae6e61
Max Marcon <maxmarcon@gmx.net>
2023-04-29 09:18:14 +0200

    prevent adding duplicate items

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,21 @@ fn sort_items(items: &mut Vec<Item>) {
     items.sort_by_key(|i| i.checked);
 }
 
-fn add_item(items: &UseStateHandle<Vec<Item>>, input_ref: &NodeRef) {
+fn add_item(
+    items: UseStateHandle<Vec<Item>>,
+    error_msg: UseStateHandle<Option<&str>>,
+    input_ref: NodeRef,
+) {
     let input = input_ref.cast::<HtmlInputElement>().unwrap();
 
     let new_item = input.value().trim().to_owned();
-    let mut updated_items = (**items).clone();
+    let mut updated_items = (*items).clone();
     if !new_item.is_empty() {
+        if items.iter().find(|&i| i.name == new_item).is_some() {
+            error_msg.set(Some("item already exists"));
+            return;
+        }
+        error_msg.set(None);
         updated_items.push(Item {
             name: new_item,
             checked: false,
@@ -33,15 +42,17 @@ fn add_item(items: &UseStateHandle<Vec<Item>>, input_ref: &NodeRef) {
 
 #[function_component]
 fn App() -> Html {
+    let error_msg = use_state(|| None);
     let items = use_state(|| Vec::<Item>::new());
     let input_ref = use_node_ref();
 
     let onkeydown = {
         let items = items.clone();
+        let error_msg = error_msg.clone();
         let input_ref = input_ref.clone();
         Callback::from(move |k: KeyboardEvent| {
             if k.key_code() == 13 {
-                add_item(&items, &input_ref);
+                add_item(items.clone(), error_msg.clone(), input_ref.clone());
             }
         })
     };
@@ -62,9 +73,10 @@ fn App() -> Html {
 
     let item_added = {
         let items = items.clone();
+        let error_msg = error_msg.clone();
         let input_ref = input_ref.clone();
         Callback::from(move |_| {
-            add_item(&items, &input_ref);
+            add_item(items.clone(), error_msg.clone(), input_ref.clone());
         })
     };
 
@@ -74,7 +86,14 @@ fn App() -> Html {
                 <ItemList items={(*items).clone()} {item_clicked} />
             </div>
             <div class="flex justify-center gap-1">
+                <div class="form-control">
                 <input type="text" class="input input-bordered" {onkeydown} ref={input_ref} />
+                <label class="label">
+                    <span class="label-text-alt text-error">
+                    {*error_msg}
+                    </span>
+                </label>
+                </div>
                 <button type="button" class="btn" onclick={item_added}>{"Add"}</button>
             </div>
         </div>
